cda838678a7f118b1a5b7f55bd5ba915aaca29f9
rmingon <ronanmingon69550@gmail.com>
2022-09-11 16:59:42 +0200

    add header

--- front/components/TheHeader.vue
+++ front/components/TheHeader.vue
@@ -1,61 +1,196 @@
 <script lang="ts" setup>
+  import { onClickOutside } from '@vueuse/core'
+  import { User } from '~~/composables/useUser';
+
   let profile_dropdown = ref(false)
+  let connected = ref<boolean>(false)
+
+  const target = ref(null)
+  onClickOutside(target, (_) => profile_dropdown.value = false)
+
+  let user = useUser()
+  const {data} = await useFetch<User>('/api/user')
+  user.value = data.value
+
+  const closePopUp = (event) => {
+    console.log("CLOSE")
+  }
+  const validateInformation = (event) => {
+    console.log("validate")
+  }
+
+  const isMenuOpen = ref(false)
+
+  const menuTabs = [
+    {title: "Houses"},
+    {title: "Me"},
+    {title: "About us"},
+  ]
 </script>
 
 <template>
-  <header class="bg-gray-800 py-4 shadow-lg">
-    <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-    <div class="relative flex h-16 items-center justify-between">
-      <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
-        <button type="button" class="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
-          <span class="sr-only">Open main menu</span>
-          <svg class="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-          </svg>
-          <svg class="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+  <PopUp v-if="false" @clickOutside="closePopUp" @asValidate="validateInformation">
+    <div class="te">
+      COUCOU
+    </div>
+  </PopUp>
+  <Modal v-if="true" @clickOutside="closePopUp" @asValidate="validateInformation">
+    <div class="">
+      COUCOU
+    </div>
+  </Modal>
+  <div class="drop-shadow-lg px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 border-b">
+    <div class="relative flex items-center justify-between">
+      <div class="flex items-center">
+        <a href="/" aria-label="Company" title="Company" class="inline-flex items-center mr-8">
+          <img src="/logo.svg" alt="" class="h-8 -mt-1">
+          <span class="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">WellKome</span>
+        </a>
+        <ul class="flex items-center hidden space-x-8 lg:flex">
+          <li>
+            <a href="/" aria-label="Our product" title="Our product" class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-violet-700" >Houses</a>
+          </li>
+          <li>
+            <a href="/" aria-label="Our product" title="Our product" class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-violet-700" >Features</a >
+          </li>
+          <li>
+            <a href="/" aria-label="Product pricing" title="Product pricing" class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-violet-700" >Pricing</a >
+          </li>
+          <li>
+            <a href="/" aria-label="About us" title="About us" class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-violet-700">About us</a>
+          </li>
+        </ul>
+      </div>
+      <ul class="flex items-center hidden space-x-8 lg:flex">
+        <li>
+          <a href="/" aria-label="Sign in" title="Sign in" class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-violet-700 border-2 border-transparent">
+            Sign in
+          </a>
+        </li>
+        <li>
+          <a href="/" class="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded bg-violet-500 shadow-md text-white hover:bg-violet-700 focus:shadow-outline focus:outline-none" aria-label="Sign up" title="Sign up">
+            Sign up
+          </a>
+        </li>
+      </ul>
+      <div class="lg:hidden">
+        <button
+          aria-label="Open Menu"
+          title="Open Menu"
+          class="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline hover:bg-deep-purple-50 focus:bg-deep-purple-50"
+          @click="isMenuOpen = true"
+        >
+          <svg class="w-5 text-gray-600" viewBox="0 0 24 24">
+            <path
+              fill="currentColor"
+              d="M23,13H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,13,23,13z"
+            ></path>
+            <path
+              fill="currentColor"
+              d="M23,6H1C0.4,6,0,5.6,0,5s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,6,23,6z"
+            ></path>
+            <path
+              fill="currentColor"
+              d="M23,20H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,20,23,20z"
+            ></path>
           </svg>
         </button>
-      </div>
-      <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-        <div class="flex flex-shrink-0 items-center">
-          <img class="block h-8 w-auto lg:hidden" src="/logo.svg" alt="welkhome">
-          <img class="hidden h-8 w-auto lg:block" src="/logo.svg" alt="welkhome">
-        </div>
-        <div class="hidden sm:ml-6 sm:block">
-          <div class="flex space-x-4">
-            <a href="#" class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">Maison</a>
-            <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Réservation</a>
-            <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Projecs</a>
-            <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Réservation</a>
-          </div>
-        </div>
-      </div>
-      <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-        <button type="button" class="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-          <span class="sr-only">View notifications</span>
-          <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
-          </svg>
-        </button>
-
-        <div class="relative ml-3">
-          <div>
-            <button @click="profile_dropdown = !profile_dropdown" type="button" class="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
-              <span class="sr-only">Open user menu</span>
-              <img class="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
-            </button>
-          </div>
-          <div v-if="profile_dropdown" class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
-            <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-0">Your Profile</a>
-            <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-1">Settings</a>
-            <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-2">Sign out</a>
+        <div v-if="isMenuOpen" class="absolute top-0 left-0 w-full">
+          <div class="p-5 bg-white border rounded shadow-sm">
+            <div class="flex items-center justify-between mb-4">
+              <div>
+                <a
+                  href="/"
+                  aria-label="Company"
+                  title="Company"
+                  class="inline-flex items-center"
+                >
+                <img src="/logo.svg" alt="" class="h-8 -mt-1">
+                  <span class="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">
+                    WellKome
+                  </span>
+                </a>
+              </div>
+              <div>
+                <button
+                  aria-label="Close Menu"
+                  title="Close Menu"
+                  class="p-2 -mt-2 -mr-2 transition duration-200 rounded hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+                  @click="isMenuOpen = false"
+                >
+                  <svg class="w-5 text-gray-600" viewBox="0 0 24 24">
+                    <path
+                      fill="currentColor"
+                      d="M19.7,4.3c-0.4-0.4-1-0.4-1.4,0L12,10.6L5.7,4.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4l6.3,6.3l-6.3,6.3 c-0.4,0.4-0.4,1,0,1.4C4.5,19.9,4.7,20,5,20s0.5-0.1,0.7-0.3l6.3-6.3l6.3,6.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3 c0.4-0.4,0.4-1,0-1.4L13.4,12l6.3-6.3C20.1,5.3,20.1,4.7,19.7,4.3z"
+                    ></path>
+                  </svg>
+                </button>
+              </div>
+            </div>
+            <nav>
+              <ul class="space-y-4">
+                <li>
+                  <a
+                    href="/"
+                    aria-label="Our product"
+                    title="Our product"
+                    class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-violet-700"
+                    >Product</a
+                  >
+                </li>
+                <li>
+                  <a
+                    href="/"
+                    aria-label="Our product"
+                    title="Our product"
+                    class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-violet-700"
+                    >Features</a
+                  >
+                </li>
+                <li>
+                  <a
+                    href="/"
+                    aria-label="Product pricing"
+                    title="Product pricing"
+                    class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-violet-700"
+                    >Pricing</a
+                  >
+                </li>
+                <li>
+                  <a
+                    href="/"
+                    aria-label="About us"
+                    title="About us"
+                    class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-violet-700"
+                    >About us</a
+                  >
+                </li>
+                <li>
+                  <a
+                    href="/"
+                    aria-label="Sign in"
+                    title="Sign in"
+                    class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-violet-700"
+                    >Sign in</a
+                  >
+                </li>
+                <li>
+                  <a
+                    href="/"
+                    class="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-violet-400 hover:bg-violet-700 focus:shadow-outline focus:outline-none active:filter-none"
+                    aria-label="Sign up"
+                    title="Sign up"
+                  >
+                    Sign up
+                  </a>
+                </li>
+              </ul>
+            </nav>
           </div>
         </div>
       </div>
     </div>
   </div>
-  </header>
 </template>
 
 <style scoped></style>
