b536dabac41970ea2080b502ac06a0137d2993dc
rmingon <ronanmingon69550@gmail.com>
2022-09-12 06:02:20 +0200

    remove nuxt pinia

--- front/nuxt.config.ts
+++ front/nuxt.config.ts
@@ -4,7 +4,6 @@ import { defineNuxtConfig } from 'nuxt'
 export default defineNuxtConfig({
     modules: [
         '@nuxtjs/tailwindcss',
-        '@pinia/nuxt',
-        '@vueuse/nuxt'
+        '@vueuse/nuxt',
     ]
 })
