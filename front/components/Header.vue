0cd2c21aa3444ae45d63dca3bfa031344b29cb65
rmingon <ronanmingon69550@gmail.com>
2022-09-29 06:35:22 +0200

    new header

---
+++ front/components/Header.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="flex flex-row w-full justify-between items-center">
+    <div class="m-4">
+      <a href="/" aria-label="Company" title="Company" class="inline-flex items-center mr-8">
+        <img src="/logo.svg" alt="" class="h-8 -mt-1">
+        <span class="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">WelKhome</span>
+      </a>
+    </div>
+    <div>
+      <input type="text" name="" id="" placeholder="Ou ?" class="border h-10 w-20 rounded-l-full pl-3 p-2 placeholder-black">
+      <input type="text" name="" id="" placeholder="Quand ?" class="border h-10 w-20 p-2 placeholder-black">
+      <input type="text" name="" id="" placeholder="Nombre ?" class="border h-10 w-20 rounded-r-full p-2 placeholder-black">
+    </div>
+    <div>
+      Login
+    </div>
+  </div>
+</template>
